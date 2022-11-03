993d29192d0ffc58cd844965f3e16a0c5a4a83f9
LiangJq <liangjianquan82@gmail.com>
2022-11-03 10:45:21 -0400

    Update header.php

--- views/header.php
+++ views/header.php
@@ -24,9 +24,9 @@ $html=' <header>
 
 <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
 <ul class="navbar-nav ml-auto">
-<li class="nav-item">
+
 <li class="nav-item"> <a class="nav-link " href="'.ROOTURL.'/logout/'.'">Log out</a> </li>
-</li>
+
 
 </ul>
 </div>
